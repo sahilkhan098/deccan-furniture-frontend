bc2fa4678ab3afa676cf058bc93c40de8d44f5e6
sahilkhan098 <itissahilkhan@gmail.com>
2026-03-20 15:32:03 +0530

    Updated axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://deccan-furniture-backend.vercel.app/api',
+  baseURL: 'https://deccan-furniture-backend.vercel.app',
   headers: {
     'Content-Type': 'application/json',
   },
